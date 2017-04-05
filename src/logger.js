8fa20c226afc23ea60c3a9bc0aa482910b7e2b0e
Alejo Fernandez <alejo.fernandez@gmail.com>
2017-04-04 23:40:47 -0300

    Build 1.0.0

--- src/logger.js
+++
@@ -1,5 +0,0 @@
-export default {
-  log: console.log,
-  debug: console.debug,
-  error: console.error
-};
--- tools/webpack/webpack.playground.config.babel.js
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-import config from './webpack.config.babel';
-
-config.output.path = path.join(__dirname, '../playground/build');
-
-export default config;
