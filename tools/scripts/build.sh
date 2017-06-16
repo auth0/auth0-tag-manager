ee3beb6781798d65209c8ec7c103566bf131391f
Nathan Totten <ntotten@auth0.com>
2017-06-16 11:16:58 -0400

    Exposed build as es5 module

--- tools/scripts/build.sh
+++ tools/scripts/build.sh
@@ -2,4 +2,5 @@
 
 NPM_BIN=$(npm bin)
 node "$NPM_BIN/webpack" --config ./tools/webpack/webpack.config.babel.js
+cp ./tools/scripts/es5.js ./es5.js
 babel src --out-dir lib
---
+++ tools/scripts/es5.js
@@ -0,0 +1,3 @@
+// This file is used to expose the ES5 module of this library
+
+module.exports = require('./build/auth0-metrics-tag-manager');
