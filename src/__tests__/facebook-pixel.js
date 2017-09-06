d4c68269c0d2eba13b1bac2b53990cff95041674
Franco Correa <fcorrea@sophilabs.com>
2017-09-06 16:39:24 -0300

    Fix code style

--- src/__tests__/facebook-pixel.js
+++ src/__tests__/facebook-pixel.js
@@ -11,11 +11,11 @@ it('handles the "track" event', () => {
       preloaded: true
     }
   });
-  
+
   window.fbq = jest.fn();
-  
+
   manager.track(eventName);
-  
+
   expect(window.fbq).toBeCalledWith('trackCustom', eventName, {});
 });
 
@@ -28,32 +28,32 @@ it('handles the "page" event', () => {
       preloaded: true
     }
   });
-  
+
   window.fbq = jest.fn();
-  
+
   manager.page();
-  
+
   expect(window.fbq).toBeCalledWith('track', 'PageView');
 });
 
 it('loads in facebook pixel', () => {
   const configureFacebookPixel = require('../facebook-pixel').default;
-  
+
   // Mock facebook pixel function
   window.fbq = jest.fn();
-  
+
   expectLoadScriptToBeCalled(configureFacebookPixel);
 });
 
 it('calls configureFacebookPixel when config is set', () => {
   const configureFacebookPixel = require('../facebook-pixel');
   configureFacebookPixel.default = jest.fn();
-  
+
   const TagManager = require('../').default;
   TagManager({
     label: 'Something',
     'facebook-pixel': {}
   });
-  
+
   expect(configureFacebookPixel.default).toBeCalled();
 });
--- src/__tests__/script-loader.js
+++ src/__tests__/script-loader.js
@@ -1,18 +1,17 @@
-
 describe('loadScript tests', () => {
   beforeEach(() => {
     // Remove <script> tags before the tests
-    document.querySelectorAll('script').forEach((tag) => {
+    document.querySelectorAll('script').forEach(tag => {
       tag.parentNode.removeChild(tag);
     });
 
     // Create <script> element for the loadScript, <script> count is 1
-    document.body.appendChild(document.createElement('script'));    
+    document.body.appendChild(document.createElement('script'));
   });
 
   it('loads a script from a URL', () => {
     const loadScript = require('../script-loader').default;
-    
+
     // Created another <script> tag
     loadScript({
       src: 'https://code.jquery.com/jquery.min.js',
@@ -20,23 +19,25 @@ describe('loadScript tests', () => {
       window,
       document,
       id: 'test',
-      stub: () => { return 'abc123'; }
+      stub: () => {
+        return 'abc123';
+      }
     });
-    
+
     // Tag count should be 2
     expect(document.querySelectorAll('script')).toHaveLength(2);
-    
+
     // A <script id="test" /> should exist
     expect(document.querySelector('script#test')).not.toBeUndefined();
-    
+
     // globalName should exist
-    expect(jQuery).not.toBeUndefined();    
-    expect(jQuery()).toEqual('abc123');    
+    expect(jQuery).not.toBeUndefined();
+    expect(jQuery()).toEqual('abc123');
   });
-  
+
   it('loads a script with stub methods from a URL', () => {
     const loadScript = require('../script-loader').default;
-    
+
     // Created another <script> tag
     loadScript({
       src: 'https://code.jquery.com/jquery.min.js',
@@ -47,29 +48,28 @@ describe('loadScript tests', () => {
       stubType: 'function',
       stubMethods: ['foo', 'bar']
     });
-    
+
     // Tag count should be 2
     expect(document.querySelectorAll('script')).toHaveLength(2);
-    
+
     // A <script id="test" /> should exist
     expect(document.querySelector('script#test')).not.toBeUndefined();
-    
-    
+
     // globalName should exist
     expect(jQuery).not.toBeUndefined();
-    
+
     // stubMethods should exist
     expect(jQuery.foo).not.toBeUndefined();
     expect(typeof jQuery.foo).toEqual('function');
     expect(jQuery.bar).not.toBeUndefined();
     expect(typeof jQuery.bar).toEqual('function');
   });
-  
-  it('errors on invalid URL', (done) => {
+
+  it('errors on invalid URL', done => {
     const loadScript = require('../script-loader').default;
-    
+
     const url = 'notAnUrl';
-    
+
     // Created another <script> tag
     loadScript({
       src: url,
@@ -77,11 +77,12 @@ describe('loadScript tests', () => {
       window,
       document,
       id: 'test',
-      stub: () => { return null; }
-    })
-    .catch((error) => {
+      stub: () => {
+        return null;
+      }
+    }).catch(error => {
       expect(error).toEqual(`Error loading ${url}`);
       done();
     });
   });
-})
+});
