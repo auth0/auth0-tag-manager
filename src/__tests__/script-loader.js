9f64e5197eb02b87aa92aa45294de2cce4815be0
Franco Correa <fcorrea@sophilabs.com>
2017-09-06 14:56:21 -0300

    Test scriptLoader stub methods exists

--- src/__tests__/script-loader.js
+++ src/__tests__/script-loader.js
@@ -12,7 +12,7 @@ it('loads a script from a URL', () => {
         window,
         document,
         id: 'test',
-        stub: () => { return null; }
+        stub: () => { return 'abc123'; }
     });
 
     // Tag count should be 2
@@ -20,6 +20,47 @@ it('loads a script from a URL', () => {
 
     // A <script id="test" /> should exist
     expect(document.querySelector('script#test')).not.toBeUndefined();
+
+    expect(jQuery).not.toBeUndefined();    
+    expect(jQuery()).toEqual('abc123');    
+
+    // Remove <script> tags after the test
+    document.querySelectorAll('script').forEach((tag) => {
+        tag.parentNode.removeChild(tag);
+    });
+});
+
+it('loads a script with stub methods from a URL', () => {
+    const loadScript = require('../script-loader').default;
+    
+    // Create <script> element for the loadScript, <script> count is 1
+    document.body.appendChild(document.createElement('script'));
+
+    // Created another <script> tag
+    loadScript({
+        src: 'https://code.jquery.com/jquery.min.js',
+        globalName: 'jQuery',
+        window,
+        document,
+        id: 'test',
+        stubType: 'function',
+        stubMethods: ['foo', 'bar']
+    });
+
+    // Tag count should be 2
+    expect(document.querySelectorAll('script')).toHaveLength(2);
+
+    // A <script id="test" /> should exist
+    expect(document.querySelector('script#test')).not.toBeUndefined();
+
+    expect(jQuery).not.toBeUndefined();
+    expect(jQuery.foo).not.toBeUndefined();
+    expect(jQuery.bar).not.toBeUndefined();
+
+    // Remove <script> tags after the test
+    document.querySelectorAll('script').forEach((tag) => {
+        tag.parentNode.removeChild(tag);
+    });
 });
 
 it('errors on invalid URL', (done) => {
@@ -28,16 +69,19 @@ it('errors on invalid URL', (done) => {
     // Create <script> element for the loadScript, <script> count is 1
     document.body.appendChild(document.createElement('script'));
 
+    const url = 'notAnUrl';
+
     // Created another <script> tag
     loadScript({
-        src: 'https://not-cdn.auth0.com/doesnotexist.js',
+        src: url,
         globalName: 'Auth0',
         window,
         document,
         id: 'test',
         stub: () => { return null; }
-    }, (error) => {
-        expect(error).not.toBeUndefined();
+    })
+    .catch((error) => {
+        expect(error).toEqual(`Error loading ${url}`);
         done();
     });
 });
