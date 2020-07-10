625b1198a014eae43c1216c89c200641ba37ec21
renetest01 <68098270+renetest01@users.noreply.github.com>
2020-07-10 16:45:07 +1000

    Update build.sh

--- tools/scripts/build.sh
+++ tools/scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NPM_BIN=$(npm bin)
+echo foo
 node "$NPM_BIN/webpack" --config ./tools/webpack/webpack.config.babel.js
 cp ./tools/scripts/es5.js ./es5.js
 node "$NPM_BIN/babel" src --out-dir lib
